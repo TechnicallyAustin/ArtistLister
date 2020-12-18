8e1b7629a74fc097bb222d340e2365868ff951b4
TechnicallyAustin <56409463+TechnicallyAustin@users.noreply.github.com>
2020-12-18 08:58:19 -0700

    Delete omniauth.rb
    Corrected policy break

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do 
-    provider :facebook, Rails.application.credentials.facebook[:app_id], Rails.application.credentials.facebook[:app_secret]
-end
